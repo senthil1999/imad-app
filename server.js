6a252f01230ca34023ad2fc4fb345d0b04f19feb
senthil1999 <30665390+senthil1999@users.noreply.github.com>
2017-08-06 11:27:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   
 });app.get('/ARTICLE-ONE',function (req,res){
-    res.send('ARTCILE-ONE is requested and will be served');
+    res.sendFile(path.join(__dirname, 'ui', 'ARTICLE-ONE.html'));
     
 });
 
